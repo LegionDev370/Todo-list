f6e8aae43aac120f31bdb253e01159785c2b90c4
LegionDev370 <llegion630@gmail.com>
2022-08-18 13:20:24 +0500

    modify

--- js/main.js
+++ js/main.js
@@ -2,22 +2,24 @@ const form = document.querySelector("form");
 const elInput = document.querySelector("#todos");
 const btn = document.querySelector("button");
 let ulLi = document.querySelector(".list-todo");
-const array = [];
+const array = ["Hello","Uzmoney"];
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
   let inputValue = elInput.value.trim();
   array.push(inputValue);
-  let elLi = document.createElement("li");
-  for (let i = 0; i < array.length; i++){
-      elLi.setAttribute("class", "list-todo-item");
-      elLi.innerHTML = `
+  ulLi.innerHTML = ""
+  console.log(array)
+  for (let i = 0; i < array.length; i++) {
+    let elLi = document.createElement("li");
+    elLi.setAttribute("class", "list-todo-item");
+    elLi.innerHTML = `
       <div class="input-checkbox">
       <input type="checkbox" id="checkbox">
       <span class="list-todo-item-text">${array[i]}</span>
       </div>
       <p class="todo-list-item-link">January 3, 2022, Monday</p>
       `;
-      ulLi.appendChild(elLi);
-    }
-    elInput.value = ""
+    ulLi.appendChild(elLi);
+  }
+  elInput.value = "";
 });
